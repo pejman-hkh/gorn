a8e7a5aeb58344007b0d20cc259b5a3fdb2b1b7a
pejman <pejman.hkh@gmail.com>
2024-03-24 22:59:31 +0330

    add auth middleware

---
+++ app/middle/middle.go
@@ -0,0 +1,32 @@
+package middle
+
+import (
+	"gorn/app/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRequired() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authUser := &model.User{}
+		if ctx.PostForm("auth") != "" || ctx.Query("auth") != "" {
+			var auth string
+			if ctx.Request.Method == "GET" {
+				auth = ctx.Query("auth")
+			} else {
+				auth = ctx.PostForm("auth")
+			}
+
+			authUser.Check(auth)
+		}
+
+		if authUser.ID != 0 {
+			ctx.Set("authUser", authUser)
+			ctx.Next()
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"status": 0, "msg": "403 access denied"})
+		ctx.Abort()
+	}
+}
