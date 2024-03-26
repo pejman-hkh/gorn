ea8fe282565d3612c0cbe36cd9c384ab62e67d44
pejman <pejman.hkh@gmail.com>
2024-03-26 12:24:32 +0330

    modify size

--- app/model/menu.go
+++ app/model/menu.go
@@ -2,11 +2,11 @@ package model
 
 type Menu struct {
 	BaseModel
-	Title    string `gorm:"size(255)"`
+	Title    string `gorm:"size:255"`
 	UserId   uint   `gorm:"index"`
 	MenuId   uint   `gorm:"index"`
-	Url      string `gorm:"size(255)"`
-	Icon     string `gorm:"size(255)"`
+	Url      string `gorm:"size:255"`
+	Icon     string `gorm:"size:255"`
 	Svg      string
 	Priority uint
 	Position uint8
