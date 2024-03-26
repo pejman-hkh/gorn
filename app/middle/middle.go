a4b3eff3526ecd265d28f4d3559cc17a402d0cf3
pejman <pejman.hkh@gmail.com>
2024-03-26 12:25:03 +0330

    seperate middle of user and admin

--- app/middle/middle.go
+++ app/middle/middle.go
@@ -6,33 +6,69 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AuthRequired() gin.HandlerFunc {
+func AccessDenied(ctx *gin.Context) {
+	ctx.JSON(403, gin.H{"status": 0, "msg": "403 access denied"})
+	ctx.Abort()
+}
+
+func IsAdmin() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		authUser := &model.User{}
-		if ctx.PostForm("auth") != "" || ctx.Query("auth") != "" {
-
-			auth := ctx.Query("auth")
-
-			if auth != "" {
-				authUser.Check(auth)
-			}
+		if !Authenticate(ctx) {
+			AccessDenied(ctx)
+			return
 		}
 
-		if authUser.ID != 0 {
-			ctx.Set("authUser", authUser)
-			if authUser.IsAdmin == 1 {
-				ctx.Next()
-			} else {
+		if get, ok := ctx.Get("authUser"); ok {
+			authUser := get.(*model.User)
+
+			if !authUser.IsAdmin {
+				AccessDenied(ctx)
+				return
+			}
+
+			if !authUser.IsMain {
+
 				route := ctx.FullPath()
 
 				if !authUser.HasPermission(route) {
-					ctx.JSON(403, gin.H{"status": 0, "msg": "403 access denied"})
-					ctx.Abort()
+					AccessDenied(ctx)
+					return
 				}
 			}
+
+		} else {
+			AccessDenied(ctx)
+		}
+
+	}
+
+}
+
+func Authenticate(ctx *gin.Context) bool {
+	authUser := &model.User{}
+	if ctx.PostForm("auth") != "" || ctx.Query("auth") != "" {
+
+		auth := ctx.Query("auth")
+
+		if auth != "" {
+			authUser.Check(auth)
+		}
+	}
+
+	if authUser.ID == 0 {
+		return false
+	}
+
+	ctx.Set("authUser", authUser)
+	return true
+}
+
+func AuthRequired() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if !Authenticate(ctx) {
+			AccessDenied(ctx)
 			return
 		}
-		ctx.JSON(403, gin.H{"status": 0, "msg": "403 access denied"})
-		ctx.Abort()
+		ctx.Next()
 	}
 }
