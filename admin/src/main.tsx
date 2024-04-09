05b027e07a1a1b5faf68756896189c2507d30cc6
pejman <pejman.hkh@gmail.com>
2024-04-09 23:55:18 +0330

    get default data from /data and set it in data context and get it in another components

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -2,9 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import Api from './router/api.tsx';
+import { useGoTo } from './router/router.tsx';
 
-Api(window.location.pathname).then(function (fetchData) {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App data={fetchData} />
-  )
+Api("/admin/data").then((mainData) => {
+  Api(window.location.pathname).then((fetchData) => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <App data={fetchData} mainData={mainData} />
+    )
+  }).catch(() => {
+    window.location.href = ('/admin/login')
+  })
 })
+
