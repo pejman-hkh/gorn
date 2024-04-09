b3613cff03a81c92f8d4faa3e550d7be3dcf5479
pejman <pejman.hkh@gmail.com>
2024-04-09 23:59:09 +0330

    some update to menu controller

--- api/app/controller/menu.go
+++ api/app/controller/menu.go
@@ -31,19 +31,29 @@ func (c *MenuController) InitRoutes(r *gin.RouterGroup) {
 	g := r.Group("admin/menus")
 	g.Use(middle.IsAdmin())
 	{
-		g.GET("/", c.Index)
+		g.GET("", c.Index)
 		g.GET("/index", c.Index)
 		g.GET("/create", c.Create)
 		g.POST("/create", c.Store)
 		g.GET("/:id/edit", c.Edit)
 		g.POST("/:id", c.Update)
 		g.DELETE("/:id", c.Destroy)
+		g.POST("/actions", c.Actions)
 	}
 }
 
 func (c *MenuController) Index(ctx *gin.Context) {
 	list := []model.Menu{}
-	c.parentIndex(ctx, list, []string{"title", "url"}, map[string]string{"title": "like", "url": "like"})
+	var p gorn.Paginator
+	search := []string{"title", "url"}
+	asearch := map[string]string{"title": "like", "url": "like"}
+	result := gorn.DB.Preload("User").Scopes(c.Search(ctx, &list, search)).Scopes(c.AdvancedSearch(ctx, &list, asearch)).Scopes(p.Paginate(ctx, &list)).Order("Id desc").Find(&list)
+	if result.Error != nil {
+		ctx.JSON(http.StatusOK, gin.H{"status": 0, "msg": result.Error})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": 1, "data": map[string]any{"list": list, "pagination": p}})
 }
 
 func (c *MenuController) Edit(ctx *gin.Context) {
@@ -106,5 +116,23 @@ func (c *MenuController) Destroy(ctx *gin.Context) {
 	menu := model.Menu{}
 	gorn.DB.First(&menu, ctx.Param("id"))
 	gorn.DB.Delete(&menu)
+	ctx.JSON(http.StatusOK, gin.H{"status": 1, "msg": "Deleted successfully"})
+}
+
+func (c *MenuController) Actions(ctx *gin.Context) {
+
+	type Actions struct {
+		Action string `form:"action" binding:"required"`
+		Ids    []uint `form:"ids[]" binding:"required"`
+	}
+	var body Actions
+	if err := ctx.ShouldBind(&body); err != nil {
+		ctx.JSON(http.StatusOK, gin.H{"status": 0, "msg": err.Error()})
+		return
+	}
+
+	ids := body.Ids
+	menu := model.Menu{}
+	gorn.DB.Delete(&menu, ids)
 	ctx.JSON(http.StatusOK, gin.H{"status": 1, "msg": "Deleted successfully"})
 }
