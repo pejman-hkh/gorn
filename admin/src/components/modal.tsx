ac751add1443263ff048f029366f72c4c3958ee3
pejman <pejman.hkh@gmail.com>
2024-04-09 23:54:21 +0330

    update modal

--- admin/src/components/modal.tsx
+++ admin/src/components/modal.tsx
@@ -1,24 +1,43 @@
 import { useRef } from "react"
 
-export function Delete({ children, ...props }: any) {
+export function ModalH3({ children, ...props }: any) {
+    return <h3 {...props} className="mt-5 mb-6 text-lg text-gray-500 dark:text-gray-400">{children}</h3>
+}
+export function NoButton({ children, ...props }: any) {
+    return <a {...props} className="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700" data-modal-hide="delete-user-modal">
+        {children}
+    </a>
+}
 
-    const modalShow = props?.show[0]?"":"hidden"
+export function YesButton({ children, ...props }: any) {
+    return <button {...props} className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-800">
+        {children}
+    </button>
+}
+export function AlertIcon() {
+    return <svg className="w-16 h-16 mx-auto text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+}
+
+export function Delete({ children, ...props }: any) {
+    const [show, setShow] = props?.show
+
+    const modalShow = show ? "" : "hidden"
     const modal = useRef(null)
     const clickHandler = (e: any) => {
         if (e.target != modal.current)
             return
 
-        props.show[1](false)
+        setShow(false)
     }
 
-    const closeHandler = (e:any) => {
+    const closeHandler = (e: any) => {
         e.preventDefault()
-        props.show[1](false)
+        setShow(false)
     }
 
 
-    return <><div modal-backdrop="1" className={modalShow+" bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40"}></div>
-        <div ref={modal} onClick={clickHandler} className={modalShow+" fixed left-0 right-0 z-50 items-center justify-center overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full flex"}>
+    return <><div modal-backdrop="1" className={modalShow + " bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40"}></div>
+        <div ref={modal} onClick={clickHandler} className={modalShow + " fixed left-0 right-0 z-50 items-center justify-center overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full flex"}>
             <div className="relative w-full h-full max-w-md px-4 md:h-auto">
                 {/* Modal content */}
                 <div className="relative bg-white rounded-lg shadow dark:bg-gray-800">
@@ -30,14 +49,7 @@ export function Delete({ children, ...props }: any) {
                     </div>
                     {/* Modal body */}
                     <div className="p-6 pt-0 text-center">
-                        <svg className="w-16 h-16 mx-auto text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                        <h3 className="mt-5 mb-6 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete this ?</h3>
-                        <a href="#" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-800">
-                            Yes, I'm sure
-                        </a>
-                        <a href="#" onClick={closeHandler} className="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700" data-modal-hide="delete-user-modal">
-                            No, cancel
-                        </a>
+                        {children}
                     </div>
                 </div>
             </div>
@@ -59,18 +71,19 @@ export function Footer({ children, ...props }: any) {
 }
 
 export function Modal({ children, ...props }: any) {
+    const [show, setShow] = props?.show
 
-    const modalShow = props?.show[0]?"":"hidden"
+    const modalShow = show ? "" : "hidden"
     const modal = useRef(null)
     const clickHandler = (e: any) => {
         if (e.target != modal.current)
             return
 
-        props.show[1](false)
+        setShow(false)
     }
 
     const closeHandler = () => {
-        props.show[1](false)
+        setShow(false)
     }
 
     return <>
