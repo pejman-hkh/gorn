91109df2ffaffb3bfc4b90516af2e0122cbc8752
pejman <pejman.hkh@gmail.com>
2024-04-11 12:17:09 +0330

    use useApiPath for download excel file

--- admin/src/components/actions.tsx
+++ admin/src/components/actions.tsx
@@ -1,3 +1,4 @@
+import { useApiPath } from "../router/api";
 import Form from "./form";
 
 export function SearchForm({ ...props }) {
@@ -24,7 +25,8 @@ export function Tasks({ children, ...props }:any) {
 
 
 export function ExportButton({ children, ...props }:any) {
-    return <a {...props} className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+    const path = useApiPath('/admin/menus')
+    return <a href={path+"&excel=true"} {...props} className="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
         <svg className="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clipRule="evenodd" /></svg>
         {children}
     </a>
