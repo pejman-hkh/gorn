1ebd47cf8f4c19f87335f3de40ae01dc144bd7fa
pejman <pejman.hkh@gmail.com>
2024-04-11 10:26:41 +0330

    add form for search

--- admin/src/components/actions.tsx
+++ admin/src/components/actions.tsx
@@ -1,3 +1,4 @@
+import Form from "./form";
 
 export function SearchForm({ ...props }) {
     const handler = (e:any) => {
@@ -6,16 +7,16 @@ export function SearchForm({ ...props }) {
         history.pushState({}, "", "?"+data);
         e.preventDefault();
     }
-    return <form {...props} onSubmit={handler} className="lg:pr-3" action="" method="GET">
+    return <Form {...props} onSubmit={handler} className="lg:pr-3" action={props.action} method="GET">
         <label htmlFor="search" className="sr-only">Search</label>
         <div className="relative mt-1 lg:w-64 xl:w-96">
             <input type="text" name="search" id="search" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search..." />
         </div>
-    </form>
+    </Form>
 }
 
 
-export function Tasks({ children, ...props }) {
+export function Tasks({ children, ...props }:any) {
     return <div {...props} className="flex pl-0 mt-3 space-x-1 sm:pl-2 sm:mt-0">
         {children}
     </div>
