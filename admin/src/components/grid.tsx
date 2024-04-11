0913c774613907a045855003fa834c8803c96ab4
pejman <pejman.hkh@gmail.com>
2024-04-11 16:21:05 +0330

    some edit

--- admin/src/components/grid.tsx
+++ admin/src/components/grid.tsx
@@ -1,5 +1,5 @@
 export function Wrapper({ children, ...props }:any) {
-    return <div {...props} className="grid grid-cols-6 gap-6">{children}</div>
+    return <div ref={props.fref} {...props} className="grid grid-cols-6 gap-6">{children}</div>
 }
 export function Col6({ children, ...props }:any) {
     return <div {...props} className="col-span-6 sm:col-span-3">{children}</div>
