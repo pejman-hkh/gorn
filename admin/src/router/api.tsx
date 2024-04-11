a192910496c6f7c4936ba1076c9b1b2908fe02a2
pejman <pejman.hkh@gmail.com>
2024-04-11 12:15:56 +0330

    add useApiPath for downloads

--- admin/src/router/api.tsx
+++ admin/src/router/api.tsx
@@ -1,3 +1,12 @@
+export function useApiPath(path: string) {
+    let url = import.meta.env.VITE_API_URL + path
+    let search = window.location.search.substr(1)
+    if (search)
+        search = '&' + search
+    let params = '?' + new URLSearchParams({ "auth": localStorage.getItem('auth') || "" }) + search
+    return url + params
+}
+
 export default function Api(path: string) {
     async function doRequest(path: string) {
         let res = {};
@@ -8,22 +17,21 @@ export default function Api(path: string) {
         let url = import.meta.env.VITE_API_URL + path
 
         if (url.substr(-1) === '/')
-            url = url.substr(0,-1)
-            
+            url = url.substr(0, -1)
+
         let search = window.location.search.substr(1)
         if (search)
             search = '&' + search
-
         let params = '?' + new URLSearchParams({ "auth": localStorage.getItem('auth') || "" }) + search
 
-        let data = await fetch(url + params, {redirect: 'follow'});
+        let data = await fetch(url + params, { redirect: 'follow' });
         if (data?.status == 200) {
             res = await data.json();
 
         }
 
         if (data?.status == 403) {
-            return Promise.reject(new Error(""+data?.status));
+            return Promise.reject(new Error("" + data?.status));
         }
 
         window.scrollTo(0, 0);
