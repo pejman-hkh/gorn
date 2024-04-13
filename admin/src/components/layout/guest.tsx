6d43e9601f4d5ca595a6cad0bb54fccc7a3273a2
pejman <pejman.hkh@gmail.com>
2024-04-13 12:08:57 +0330

    add direction to guest

--- admin/src/components/layout/guest.tsx
+++ admin/src/components/layout/guest.tsx
@@ -1,6 +1,11 @@
+import { useContext } from "react"
+import { DataContext } from "../../router/data"
 
-export default function Geust({children}:any) {
-    return <main className="bg-gray-50 dark:bg-gray-900">
-        {children}
+export default function Geust({ children }: any) {
+  const dataContext = useContext(DataContext) as Array<any>
+  const direction = dataContext[4]
+
+  return <main dir={direction[0]} className="bg-gray-50 dark:bg-gray-900">
+    {children}
   </main>
 }
