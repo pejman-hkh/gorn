7cabb4c1caf58f0d05f3ead487f52ff28063b7cc
pejman <pejman.hkh@gmail.com>
2024-04-18 15:25:37 +0330

    add route for pages

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -4,6 +4,7 @@ import * as Menus from './pages/menus'
 import * as Groups from './pages/groups'
 import * as Users from './pages/users'
 import * as Settings from './pages/settings'
+import * as Pages from './pages/pages'
 import Dashboard from './pages/dashboard'
 import NoPage from './pages/nopage'
 import Login from './pages/login'
@@ -19,6 +20,7 @@ import useRouter from "./router/router";
 let routes = {
   '/': [Guest, Login],
   '/menus': [Layout, Menus.Index],
+  '/pages': [Layout, Pages.Index],
   '/groups': [Layout, Groups.Index],
   '/users': [Layout, Users.Index],
   '/settings': [Layout, Settings.Index],
