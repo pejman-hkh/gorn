1644c479ccc6131f9e87133fc9c1da602c930245
pejman <pejman.hkh@gmail.com>
2024-04-18 15:24:51 +0330

    change user helper model for no conflict with main user model

--- api/app/hmodel/user.go
+++ api/app/hmodel/user.go
@@ -2,6 +2,6 @@ package hmodel
 
 type User struct {
 	ID    uint   `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name  string `json:"name" gorm:"size:255"`
+	Email string `json:"email" gorm:"type:varchar(100);index:idx_email"`
 }
