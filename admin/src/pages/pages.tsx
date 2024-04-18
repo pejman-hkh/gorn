247f317664fd47ce57a3c8339edbac0425bf9528
pejman <pejman.hkh@gmail.com>
2024-04-18 15:25:50 +0330

    add pages page

---
+++ admin/src/pages/pages.tsx
@@ -0,0 +1,136 @@
+import Pagination from "../components/pagination"
+import * as List from "../components/list"
+import * as Grid from "../components/grid"
+import Form, { Input, Select, Editor, Textarea } from "../components/form"
+import { useContext, useRef, useState } from "react"
+import { DataContext } from "../router/data"
+import Link from "../router/link"
+import DateTime from "../components/date"
+import { useTranslation } from 'react-i18next';
+
+export function MainForm({ ...props }) {
+    let edit = props.edit
+    const { t } = useTranslation();
+
+    return <Grid.Wrapper key={edit?.id} {...props}>
+        <Grid.Col6>
+            <Input type="text" name="title" defaultValue={edit?.title}>{t("Title")}</Input>
+        </Grid.Col6>
+        <Grid.Col6>
+            <Input type="text" name="url" defaultValue={edit?.url}>{t("Url")}</Input>
+        </Grid.Col6>
+  
+        <Grid.Col6>
+            <Select name="status" title={t("Status")} defaultValue={edit?.status}>
+                <option value="1">{t("Enable")}</option>
+                <option value="0">{t("Disable")}</option>
+            </Select>
+        </Grid.Col6>
+
+        <Grid.Col6>
+            <Select name="has_comment" title={t("Has Comment")} defaultValue={edit?.has_comment}>
+                <option value="1">{t("Has")}</option>
+                <option value="0">{t("Doesn't have")}</option>
+            </Select>
+        </Grid.Col6>
+
+        <Grid.Span6>
+            <Editor name="content" title={t("Content")} defaultValue={edit?.content||""}  />
+        </Grid.Span6>
+
+    </Grid.Wrapper>
+}
+
+export function Index() {
+
+    const route = "/pages"
+    const title = "Page"
+
+    const dataContext = useContext(DataContext) as Array<any>
+    const data = dataContext[0]
+
+    const [edit, setEdit] = useState<any>({})
+    const editModal = useState(false)
+    const searchModal = useState(false)
+    const addModal = useState(false)
+    const deleteModal = useState(false)
+    const deleteAllModal = useState(false)
+    const [actionValue, setActionValue] = useState("")
+
+    const editHandler = (e: any, item: any) => {
+        e.preventDefault()
+        editModal[1](true)
+        setEdit(item)
+    }
+
+    const listForm = useRef<any>(null)
+
+    const { t } = useTranslation();
+ 
+    return <>
+        <List.BreadCrumb {...{ title, route, setActionValue, deleteAllModal, addModal, setEdit, searchModal }}></List.BreadCrumb>
+
+        <Form fref={listForm} disableclass="true" method="post" action={route + "/actions"} alertclass="m-6">
+            <input type="hidden" name="action" value={actionValue} />
+
+            <List.Table>
+                <List.Thead>
+                    <tr>
+                        <List.Th>
+                            <List.Checkbox />
+                        </List.Th>
+                        <List.Th width="20%">{t("Title")}</List.Th>
+                        <List.Th>{t("User")}</List.Th>
+                        <List.Th>{t("Url")}</List.Th>
+                        <List.Th>{t("Status")}</List.Th>
+                        <List.Th>{t("Date")}</List.Th>
+                        <List.Th width="5%">{t("Actions")}</List.Th>
+                    </tr>
+                </List.Thead>
+                <List.Tbody>
+                    {data?.data?.list?.map((item: any) => (
+                        <List.Tr key={item.id}>
+                            <List.TdCheckbox>
+                                <List.CheckboxTd name="ids[]" value={item.id} />
+                            </List.TdCheckbox>
+
+                            <List.TdTitle>
+                                {item.title}
+                            </List.TdTitle>
+
+                 
+                            <List.Td><Link to={"/users?id=" + item?.user?.id}>{item?.user?.name}</Link></List.Td>
+                
+                            <List.TdText>
+                                {item.url}
+                            </List.TdText>
+
+
+                            <List.Td>
+                                <List.ActiveBadge active={item?.status} />
+                            </List.Td>
+                            <List.Td>
+                                <DateTime time={item.created_at} />
+                                <br />
+                                {item.updated_at != item.created_at ? <DateTime time={item.updated_at} /> : ""}
+                            </List.Td>
+                            <List.TdAction>
+                                <List.ButtonEdit onClick={(e: any) => editHandler(e, item)}>
+
+                                </List.ButtonEdit>
+                                <List.ButtonDelete onClick={() => { deleteModal[1](true); setEdit(item) }}>
+
+                                </List.ButtonDelete>
+                            </List.TdAction>
+                        </List.Tr>
+                    ))}
+
+                </List.Tbody>
+            </List.Table>
+        </Form>
+        <Pagination pagination={data?.data?.pagination} module={route}></Pagination>
+
+        <List.Modals {...{ title, route, edit, MainForm, addModal, editModal, searchModal, deleteModal, deleteAllModal, listForm }}></List.Modals >
+
+    </>
+}
