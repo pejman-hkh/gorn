80a16d71461f24434cf7ce43a4bbb45eee2ecefc
pejman <pejman.hkh@gmail.com>
2024-04-18 15:26:21 +0330

    helper user model for menu

--- api/app/model/menu.go
+++ api/app/model/menu.go
@@ -1,15 +1,17 @@
 package model
 
+import "gorn/app/hmodel"
+
 type Menu struct {
 	BaseModel
-	Title    string `gorm:"size:255" json:"title"`
-	UserId   uint   `gorm:"index" json:"user_id"`
-	MenuId   uint   `gorm:"index" json:"menu_id"`
-	Url      string `gorm:"size:255" json:"url"`
-	Icon     string `gorm:"size:255" json:"icon"`
-	Svg      string `json:"svg"`
-	Priority uint   `json:"priority"`
-	Position uint8  `json:"position"`
-	Status   uint8  `json:"status"`
-	User     User   `json:"user"`
+	Title    string      `gorm:"size:255" json:"title"`
+	UserId   uint        `gorm:"index" json:"user_id"`
+	MenuId   uint        `gorm:"index" json:"menu_id"`
+	Url      string      `gorm:"size:255" json:"url"`
+	Icon     string      `gorm:"size:255" json:"icon"`
+	Svg      string      `json:"svg"`
+	Priority uint        `json:"priority"`
+	Position uint8       `json:"position"`
+	Status   uint8       `json:"status"`
+	User     hmodel.User `json:"user"`
 }
